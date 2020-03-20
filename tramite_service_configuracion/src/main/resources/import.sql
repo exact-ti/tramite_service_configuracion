5896fa98de4335b618b17d6cf9a28d4a08688e9a
Orlando <oheredia20122063@gmail.com>
2020-03-19 21:32:43 -0500

    Adición de cantidad de resultados

--- tramite_service_configuracion/src/main/resources/import.sql
+++ tramite_service_configuracion/src/main/resources/import.sql
@@ -1,1 +1,1 @@
-insert into configuracion(clave, valor) values('CARACTERES_MINIMOS_BUSQUEDA', '3'), ('TEMA', 'AZUL');
+insert into configuracion(clave, valor) values('CARACTERES_MINIMOS_BUSQUEDA', '3'), ('TEMA', 'AZUL'), ('CANTIDAD_RESULTADOS', '10');
